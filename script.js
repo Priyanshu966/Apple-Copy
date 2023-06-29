a8b33cf9d76346123ce9e7731549a3bf00dc4743
priyanshu966 <pri22204901@gmail.com>
2023-06-29 15:03:22 +0530

    Apple project

---
+++ script.js
@@ -0,0 +1,33 @@
+//For toggling menubar
+
+let btnToggle = document.querySelectorAll('.btn-toggle');
+let hamBurger = document.querySelector('.hide-lines');
+let xMark = document.querySelector('.xmark');
+let logoCont = document.querySelector('.logo-cont');
+let aBag = document.querySelector('.a-bag');
+let aGlass = document.querySelector('.a-glass');
+let navCenter = document.querySelector('.nav-center');
+let main = document.querySelector('main');
+let header = document.querySelector('header');
+let footer = document.querySelector('footer');
+let dropBar = document.querySelector('.drop-bar');
+
+
+btnToggle.forEach(function(e){
+    e.addEventListener('click' , function(){
+        hamBurger.classList.toggle('hide');
+        logoCont.classList.toggle('hide');
+        aBag.classList.toggle('hide');
+        aGlass.classList.toggle('hide');
+        navCenter.classList.toggle('flex-end');
+        main.classList.toggle('hide');
+        footer.classList.toggle('hide');
+        header.classList.toggle('pos-rel');
+        for(let i = 1; i <= 5; i++){
+            document.querySelector(`.sec${i}`).classList.toggle('hide');
+        }
+        xMark.classList.toggle('show');
+        dropBar.classList.toggle('show');
+        dropBar.classList.toggle('height');
+    })
+})
