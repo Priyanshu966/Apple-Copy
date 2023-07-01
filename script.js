fd2480c8dc83da3ba0c5924f136aaac665105df0
Priyanshu <134921189+Priyanshu966@users.noreply.github.com>
2023-07-01 20:52:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ btnToggle.forEach(function(e){
         main.classList.toggle('hide');
         footer.classList.toggle('hide');
         header.classList.toggle('pos-rel');
-        for(let i = 1; i <= 5; i++){
+        for(let i = 1; i <= 6; i++){
             document.querySelector(`.sec${i}`).classList.toggle('hide');
         }
         xMark.classList.toggle('show');
@@ -42,4 +42,63 @@ rightCont.forEach(function(e){
     e.addEventListener('mouseout' , function(){
         e.lastElementChild.classList.remove('show-right');
     })
+})
+
+//For toggling footer links
+
+let linkToggle = document.querySelectorAll('.footer-link h6');
+
+linkToggle.forEach(function(e){
+    e.addEventListener('click' , function(){
+        e.classList.toggle('border-hide');
+        e.nextElementSibling.classList.toggle('show-link');
+        e.lastElementChild.classList.toggle('rotate');
+    })
+})
+
+//For toggling slider buttons and slider 
+
+let slides = document.querySelectorAll('.img-cont');
+let btns = document.querySelectorAll('.btn');
+let playBtn = document.querySelector('.play-btn');
+let play = document.querySelector('.play-btn .fa-play');
+let pause = document.querySelector('.play-btn .fa-pause');
+
+let count = 0;
+
+slides.forEach(function(slide , index){
+    slide.style.left = `${100 * index}%`;
+
+})
+let start;
+function startInterval(){
+        start = setInterval(function(){
+        count++
+        slides.forEach(function(slide){
+            slide.style.transform =  `translateX(${-100 * count}%)`
+        })
+        if(count == slides.length){
+            count = 0;
+        }
+        for(x of btns){
+            x.classList.remove('black-bag');
+        }
+        btns[count].classList.add('black-bag');
+    }, 5000);
+}
+
+startInterval();
+let stop = true;
+playBtn.addEventListener('click' , function(){
+    pause.classList.toggle('hide');
+    play.classList.toggle('show');
+    if(stop == true){
+        clearInterval(start);
+        stop = false;
+    }else{
+        startInterval();
+        stop = true;
+        
+    }
+
 })
